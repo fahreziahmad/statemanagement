eee1ec5939d125515759b59c1892e20b886b4c57
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-04-03 13:00:20 +0700

    menambahkan provider untuk state nama dan email

---
+++ lib/provider.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+//PROVIDER
+final namaProvider = StateProvider<String>((ref) => "");
+final emailProvider = StateProvider<String>((ref) => "");
