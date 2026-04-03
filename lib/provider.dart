be132e979fad5a845d436bc507bcbfbdee7e0c85
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-04-03 13:02:44 +0700

    menambahkan UI FormPage dengan ConsumerWidget

--- lib/provider.dart
+++ lib/provider.dart
@@ -23,3 +23,28 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+//UI
+class FormPage extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final nama = ref.watch(namaProvider);
+    final email = ref.watch(emailProvider);
+
+    return Scaffold(
+      appBar: AppBar(title: Text("Form Riverpod")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: "Nama"),
+              onChanged: (value) =>
+                  ref.read(namaProvider.notifier).state = value,
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: "Email"),
+              onChanged: (value) =>
+                  ref.read(emailProvider.notifier).state = value,
+            ),
+           
