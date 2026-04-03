78dbdd0a7ae0d164f1078068b86f2ad9c5318ed1
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-04-03 13:01:34 +0700

    menambahkan konfigurasi MyApp dan routing awal

--- lib/provider.dart
+++ lib/provider.dart
@@ -12,3 +12,14 @@ void main() {
     ),
   );
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FormPage(),
+    );
+  }
+}
